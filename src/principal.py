1ac3d0ce8b7ca6ce6b75f203cb17ebfac09c3da1
Beto Schneider <roberto.schneider@gmail.com>
2025-02-04 23:04:53 -0300

    Atualização: Projeta valores recorrentes para os meses seguintes

--- src/principal.py
+++ src/principal.py
@@ -3,8 +3,9 @@ import os
 import sys
 import streamlit as st
 import sqlite3
-from datetime import datetime
+from datetime import datetime, timedelta
 import pandas as pd
+import numpy as np
 
 # Adiciona o diretório raiz ao path do Python
 current_dir = os.path.dirname(os.path.abspath(__file__))
@@ -97,42 +98,98 @@ def main():
                 "Status": "Efetivado" if t.is_completed else "Pendente",
                 "Recorrente": "Sim" if t.is_recurring else "Não"
             })
+        # Definindo as datas atuais
+        data_atual = datetime.now()
+        mes_atual = data_atual.month
+        ano_atual = data_atual.year
+
+        # Definindo as datas da referência anterior
+        data_mes_anterior = data_atual.replace(day=1) - timedelta(days=1)
+        mes_data_anterior = data_mes_anterior.strftime("%m")
+        ano_data_anterior = data_mes_anterior.strftime("%Y")
 
         # Tratamento do histórico de transações
         data = pd.DataFrame(data)
         data["Ano"] = data["Ano"].astype(int)
         data["Ref"] = data["Ref"].astype(int)
-        # Seleciona o ano desejado
+
+        # Dataframe com transações recorrentes da referência anterior
+        transacoes_recorrentes = data[(data["Ref"] == int(mes_data_anterior)) & (data["Ano"] == int(ano_data_anterior)) & (data["Recorrente"] == "Sim")]
+        transacoes_recorrentes = transacoes_recorrentes.drop("Ano", axis=1)
+
+        # Seleciona as transações do ano desejado
         data = data[data["Ano"] == int(selected_transaction_year)] 
         data = data.drop("Ano", axis=1)
-        # Busca o último mês com transação
-        max_mes = data["Ref"].max()
-        # DF temporário com as transações recorrentes do último mês
-        transacoes_recorrentes = data[(data["Ref"] == max_mes) & (data["Recorrente"] == "Sim")]
-        # Adiciona as transações recorrentes para os meses seguintes
-        for i in range(12 - max_mes):
-            transacoes_recorrentes["Ref"] = i + max_mes + 1
-            data = pd.concat([data, transacoes_recorrentes], ignore_index=True)
+
+        # Se o ano selecionado for o atual, projeta as transações recorrentes para os meses seguintes
+        if ano_atual == int(selected_transaction_year):
+            # Adiciona as transações recorrentes para os meses seguintes
+            for i in range(13 - mes_atual):
+                transacoes_recorrentes["Ref"] = i + mes_atual
+
+                # Cria uma chave de identificação única
+                transacoes_recorrentes['chave'] = transacoes_recorrentes['Item'] + transacoes_recorrentes['Ref'].astype(str)
+                data['chave'] = data['Item'] + data['Ref'].astype(str)
+
+                # Atualiza usando merge
+                transacoes_recorrentes = (
+                    transacoes_recorrentes.merge(
+                        data,
+                        on='chave',
+                        how='left',
+                        suffixes=('', '_novo')
+                    )
+                )
+
+                # Atualiza as colunas onde há correspondência
+                colunas = ['Ref', 'Data', 'Item','Categoria', 'Valor', 'Tipo', 'Status', 'Recorrente']
+                for col in colunas:
+                    mascara = ~transacoes_recorrentes[f'{col}_novo'].isna()
+                    transacoes_recorrentes.loc[mascara, col] = transacoes_recorrentes.loc[mascara, f'{col}_novo']
+
+                # Remove as colunas temporárias
+                transacoes_recorrentes = transacoes_recorrentes.drop([col + '_novo' for col in colunas] + ['chave'], axis=1)
+
+                # Concatena os dataframes
+                data = pd.concat([data, transacoes_recorrentes], ignore_index=True)
+
+        # Remove duplicatas
+        data.drop_duplicates(inplace=True)
+
+        # Trata possíveis erros e garante a conversão correta da coluna Valor para numérico
+        data['Valor'] = pd.to_numeric(
+            data['Valor'].str.replace('R$', '').str.strip(),
+            errors='coerce'
+        )
+
+        # Multiplica o valor por -1 se o tipo for Débito
+        data['Valor'] = np.where(data['Tipo'] == 'Débito', data['Valor'] * -1, data['Valor'])
+
         # Pivotando a tabela: linhas itens, colunas mês
-        data = pd.pivot_table(
+        df_exibicao = pd.pivot_table(
             data,
             values="Valor",
             index="Item",
             columns="Ref",
             aggfunc='sum'
         ).reset_index()
-        # Ajustando nome dos meses
-        colunas = data.columns
+
+        # Ajustando nome dos meses da tabela pivotada
+        colunas = df_exibicao.columns
         meses = [
             "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", 
             "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
         ]
         novas_colunas = [colunas[0]] + [meses[int(num) - 1] for num in colunas[1:]]
-        data.columns = novas_colunas
+        df_exibicao.columns = novas_colunas
+
+        # Formatação dos valores para exibição
+        for num in colunas[1:]:
+            df_exibicao[meses[int(num) - 1]] = df_exibicao[meses[int(num) - 1]].apply(lambda x: f"{x:,.2f}".replace(',', '_').replace('.', ',').replace('_', '.') if pd.notna(x) else "")
 
         # Exibindo as informações na tabela
         st.dataframe(
-            data,
+            df_exibicao,
             use_container_width=True,
             hide_index=True
         )
